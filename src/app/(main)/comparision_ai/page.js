9937197400bd78ee115a4d5ecf42812b71d3189e
BHAVYA <bhavy1921@gmail.com>
2026-03-03 13:56:15 +0530

    feat(ai-ui): update AI comparison main page structure

--- src/app/(main)/comparision_ai/page.js
+++ src/app/(main)/comparision_ai/page.js
@@ -2,22 +2,14 @@ import Comparision_AI_Client from "@/components/AI_Components/Client_Ai";
 
 export default async function Comparision_AI({ searchParams }) {
 
- const resolvedSearchParams = await searchParams;
+  const resolvedSearchParams = await searchParams;
 
-// const ids = resolvedSearchParams?.ids
-//     ? resolvedSearchParams.ids.split(",")
-//     : [];
+  const ids = resolvedSearchParams?.ids
+    ? resolvedSearchParams.ids.split(",")
+    : [];
 
-const ids = [
-    "15-ec2145ax-699e8c",
-    "ideapad-gaming-3-57bfa3",
-    "yoga-6-078f44",
-    
-
-  ];
-
-   
-    console.log("ids",ids)
+  console.log("ids from URL:", ids);
+  console.log("Server received ids:", resolvedSearchParams?.ids);
 
   return <Comparision_AI_Client ids={ids} />;
 }
