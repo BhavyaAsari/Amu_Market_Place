7c839f1cd4d8bcffdadc67c547cd5f4ed8bd5eb9
BHAVYA <bhavy1921@gmail.com>
2026-03-09 10:11:29 +0530

    Enhance brand section with updated metadata and UI improvements

--- src/components/productComponents/brandSection.js
+++ src/components/productComponents/brandSection.js
@@ -1,40 +1,124 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  LuBriefcase,
+  LuGamepad2,
+  LuWallet,
+  LuLayoutGrid,
+} from "react-icons/lu";
 
 export default function BrandSection({ brands }) {
+
+  const tabs = ["all", "business", "gaming", "budget"];
+   const tabIcons = {
+  all: LuLayoutGrid,
+  business: LuBriefcase,
+  gaming: LuGamepad2,
+  budget: LuWallet,
+};
+  const [activeTab, setActiveTab] = useState("all");
+
+ const filteredBrands = brands.filter((brand) => {
+  if (activeTab === "all") return true;
+
+  if (Array.isArray(brand.categories)) {
+    return brand.categories.includes(activeTab);
+  }
+
+  return brand.categories === activeTab;
+});
   return (
-    <section className="productContainer ">
-      {brands.map((brand, index) => (
-        <div key={index} className="brandContainer group relative">
-         <div className="CardMask"></div>
+    <>
+      <div className="ui-header mb-8">
 
-         <div className="BrandImageContainer ">
+        <h2 className="title-headerSec">
+          Explore your choice of device
+        </h2>
 
-            <Image
-            src={brand.image}
-            alt={brand.name}
-            width={180}
-            height={80}
-            className="ImageCard"
-          />
+        <div className="ui-tab-group ">
+{tabs.map((tab) => {
+  const Icon = tabIcons[tab];
+   const isActive = activeTab === tab;
 
-          <Link href={`productlist?brand=${brand.name.toLowerCase()}`}><button className="BrandButton">View</button></Link>
+  return (
+    <button
+      key={tab}
+      onClick={() => setActiveTab(tab)}
+      className={`ui-tab flex items-center gap-2 text-lg ${
+        activeTab === tab ? "ui-tab-active" : ""
+      }`}
+    >
+
+   
+      {isActive ? (
+        <Icon  size={20} />
+      ) : (
+        tab.charAt(0).toUpperCase() + tab.slice(1)
+      )}
 
 
-         </div>
-         <div className="mt-2 text-center  ">
-             <p className="text-xl sm:text-2xl font-bold  text-zinc-600">
-            {brand.name}
-          </p>
+    </button>
+  );
+})}
 
-          <p className="sm:text-xl text-sm opacity-70  ">
-            {brand.tagline}
-          </p>
-         </div>
         </div>
-      ))}
-    </section>
+      </div>
+
+      <section className="productContainer">
+
+        {filteredBrands.map((brand, index) => (
+
+          <div key={index} className="brandContainer group relative"
+          style={{ borderTop: `6px solid ${brand.color}` }}>
+
+            <div className="CardMask"></div>
+
+            <div className="BrandImageContainer">
+
+              <Image
+                src={brand.image}
+                alt={brand.name}
+                width={180}
+                height={80}
+                className="ImageCard"
+              />
+
+              <Link href={`productlist?brand=${brand.name.toLowerCase()}`}>
+                <button className="BrandButton">
+                  View
+                </button>
+              </Link>
+
+            </div>
+
+            {/* <div className="relative w-30 h-30 brandContainer group  overflow-hidden">
+              <Image
+              src={brand.logo} 
+              alt={brand.name}
+              fill
+              className="object-contain"/>
+            </div> */}
+
+            <div className="mt-2 text-center">
+
+              <p className="text-xl sm:text-2xl font-bold text-zinc-600">
+                {brand.name}
+              </p>
+
+              <p className="sm:text-xl text-sm opacity-70">
+                {brand.tagline}
+              </p>
+
+            </div>
+
+          </div>
+
+        ))}
+
+      </section>
+    </>
   );
 }
--- src/config/brandMeta.js
+++ src/config/brandMeta.js
@@ -2,74 +2,127 @@ export const brandMeta = {
   acer: {
     name: "Acer",
     tagline: "Affordable Performance & Everyday Power",
-    image: "/products/acer/acer_1.png",
+    image: "/products/acer/acer.png",
+    categories: ["budget", "gaming"],
+    color: "#83B81A",
+    logo:"/logos/accerlogo.png"
   },
+
   apple: {
     name: "Apple",
     tagline: "Creative Power for Professionals",
-    image: "/products/apple/mac.png",
+    image: "/products/apple/apple.png",
+    categories: ["business", "premium"],
+    color: "#000000",
+    logo:"/logos/applelogo.png"
   },
+
   asus: {
     name: "ASUS",
     tagline: "Innovation Meets Gaming Excellence",
     image: "/products/asus/asus.png",
+    categories: ["gaming"],
+    color: "#0071C5",
+    logo:"/logos/asuslogo.png"
   },
+
   avita: {
     name: "Avita",
     tagline: "Stylish Laptops for Modern Users",
     image: "/products/avita/avita.png",
+    categories: ["budget"],
+    color: "#7A3CF0",
+    logo:"/logos/avitalogo.png"
   },
+
   dell: {
     name: "Dell",
     tagline: "Reliable Business & Productivity Machines",
     image: "/products/dell/dell.png",
+    categories: ["business"],
+    color: "#007DB8",
+    logo:"/logos/delllogo.png"
   },
+
   hp: {
     name: "HP",
     tagline: "Design, Security & Everyday Reliability",
     image: "/products/hp/hp.png",
+    categories: ["business", "student"],
+    color: "#0096D6",
+    logo:"/logos/hplogo.png"
   },
+
   infinix: {
     name: "Infinix",
     tagline: "Smart Performance for Budget Users",
     image: "/products/infinix/infinix.png",
+    categories: ["budget"],
+    color: "#000000",
+    logo:"/logos/infilixlogo.png"
   },
+
   lenovo: {
     name: "Lenovo",
     tagline: "Productivity & Performance for All",
     image: "/products/thinkbook/think2.png",
+    categories: ["business", "budget"],
+    color: "#E2231A",
+    logo:"/logos/lenovologo.png"
   },
+
   lg: {
     name: "LG",
     tagline: "Premium Design with Cutting-Edge Displays",
     image: "/products/lg/lg.png",
+    categories: ["business"],
+    color: "#A50034",
+    logo:"/logos/lglogo.png"
   },
+
   msi: {
     name: "MSI",
     tagline: "High-Performance Gaming & Creator Laptops",
     image: "/products/msi/msi.png",
+    categories: ["gaming"],
+    color: "#E2231A",
+    logo:"/logos/msilogo.png"
   },
+
   realme: {
     name: "Realme",
     tagline: "Value-Driven Performance for Young Users",
     image: "/products/realme/realme.png",
+    categories: ["budget"],
+    color: "#FFC915",
+    logo:"/logos/realmelogo.png"
   },
+
   redmibook: {
     name: "RedmiBook",
     tagline: "Powerful Specs at Smart Prices",
     image: "/products/redmi/redmi.png",
+    categories: ["budget"],
+    color: "#FF6900",
+    logo:"/logos/redmibooklogo.png"
   },
-  
+
   samsung: {
     name: "Samsung",
     tagline: "Premium Ecosystem & Future-Ready Tech",
     image: "/products/samsung/samsung.png",
+    categories: ["business"],
+    color: "#1428A0",
+    logo:"/logos/samsunglogo.png"
   },
 
-   vaio: {
+  vaio: {
     name: "VAIO",
     tagline: "Precision Engineering & Professional Craft",
     image: "/products/vaio/vaio.png",
-  },
-  
+    categories: ["business"],
+    color: "#000000",
+    logo:"/logos/vivaologo.png"
+
+  }
 };
