d1938da83f59fcfa8072d8c608cfc09c01374492
BHAVYA <bhavy1921@gmail.com>
2026-03-11 16:43:37 +0530

    update: admin dashboard page

--- src/app/(admin)/admin/dashboard/page.js
+++ src/app/(admin)/admin/dashboard/page.js
@@ -1,6 +1,7 @@
 import { getProductStats } from "@/app/actions/adminActions/getProductStats";
 import { getRevenueData } from "@/app/actions/adminActions/getRevenueData";
 import { getUsersDetails } from "@/app/actions/adminActions/getUserDetails";
+import { getUserStats } from "@/app/actions/adminActions/getUserStats";
 import AdminLayout from "@/components/admin/AdminLayOut";
 
 export default async function AdminDashBoard() {
@@ -8,12 +9,16 @@ export default async function AdminDashBoard() {
   const stats = await getProductStats();
   const revenueData = await getRevenueData();
   const users = await getUsersDetails();
+  const userStats = await getUserStats();
+
+  console.log("user Stats",userStats)
 
   return (
     <AdminLayout
       stats={stats}
       revenueData={revenueData}
       users = {users}
+      userStats={userStats}
     />
   );
 }
