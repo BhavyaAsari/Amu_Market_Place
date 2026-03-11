127f6665967f6a79cfe58f2d39f5ac213615155d
BHAVYA <bhavy1921@gmail.com>
2026-03-11 16:43:30 +0530

    update: edit user page

---
+++ src/app/(admin)/admin/Users/edit/[id]/page.js
@@ -0,0 +1,22 @@
+import EditUserForm from "@/components/admin/UserSegment/editUserForm";
+import { connectDB } from "@/libs/db";
+import User from "@/models/Users";
+
+export default async function UserEdit({ params }) {
+
+  const { id } = await params;
+
+  await connectDB();
+
+  const user = await User.findById(id).lean();
+
+  if (!user) {
+    return <p>User not found</p>;
+  }
+
+  return (
+    <main>
+      <EditUserForm user={JSON.parse(JSON.stringify(user))} />
+    </main>
+  );
+}
