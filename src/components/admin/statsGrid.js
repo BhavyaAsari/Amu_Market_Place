4c3173222ed86a24d34dced563b28c913307814f
BHAVYA <bhavy1921@gmail.com>
2026-03-11 16:44:37 +0530

    update: stats grid component

--- src/components/admin/statsGrid.js
+++ src/components/admin/statsGrid.js
@@ -12,14 +12,14 @@ export default function StatsGrid({ items }) {
           return (
             <div
               key={i}
-              className="admin-stats-card hover-effect"
+              className="admin-stats-card hover-effect border"
             >
               <div className="flex flex-col ml-2">
-                <p className="font-semibold text-gray-500 text-sm">
+                <p className="font-semibold text-purple-300 text-sm">
                   {item.label}
                 </p>
 
-                <h3 className="text-2xl text-purple-600">
+                <h3 className="text-xl text-white ">
                   {item.value}
                 </h3>
               </div>
