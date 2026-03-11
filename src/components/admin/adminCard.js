c9fb7793fef4801e24a6ed41ddda08207aa4a403
BHAVYA <bhavy1921@gmail.com>
2026-03-11 16:44:26 +0530

    update: admin card component

--- src/components/admin/adminCard.js
+++ src/components/admin/adminCard.js
@@ -1,13 +1,13 @@
 "use client";
 
-export default function AdminCard({children}) {
+export default function AdminCard({children ,bgColor=""}) {
 
 
     return (
 
         <>
         
-        <main className="admin-card">
+        <main className={`admin-card ${bgColor}`}>
 
             {/* <p>Reusbale Admin Card</p> */}
             {children}
