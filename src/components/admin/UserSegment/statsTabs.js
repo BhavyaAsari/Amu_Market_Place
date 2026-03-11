24d2bf564dcc824e47bcb7ed662b48998cc8b8c8
BHAVYA <bhavy1921@gmail.com>
2026-03-11 16:44:04 +0530

    update: user stats tabs

--- src/components/admin/UserSegment/statsTabs.js
+++ src/components/admin/UserSegment/statsTabs.js
@@ -1,7 +1,34 @@
-export default function StatsTabUser() {
+import AdminCard from "../adminCard";
+import StatsGrid from "../statsGrid";
+import { LuUser, LuBan, LuUserPlus } from "react-icons/lu"
+
+export default function StatsTabUser({userStats}) {
+
+    const items = [
+    {
+      label: "Active Users",
+      value: userStats.activeUsers,
+      icon: LuUser
+    },
+    {
+      label: "Blocked Users",
+      value: userStats.blockedUsers,
+      icon: LuBan
+    },
+    {
+      label: "New Signups",
+      value: userStats.newSignups,
+      icon: LuUserPlus
+    }
+  ];
 
     return (
 
-        sra
+    
+   <AdminCard  bgColor="bg-gradient-to-r from-[#4c1d95] via-[#6d28d9] to-[#9333ea]">
+       <h1 className="font-semibold text-white text-xl mb-3">User Insights</h1>
+      <StatsGrid  items={items}/>
+   </AdminCard>
+     
     )
 }
