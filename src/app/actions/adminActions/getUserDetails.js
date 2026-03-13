bbb39e85ea1349d021ddbbb70220511c9b46506c
BHAVYA <bhavy1921@gmail.com>
2026-03-13 16:20:49 +0530

    feat(admin-api): implement user details retrieval action

--- src/app/actions/adminActions/getUserDetails.js
+++ src/app/actions/adminActions/getUserDetails.js
@@ -3,27 +3,43 @@
 import { connectDB } from "@/libs/db";
 import User from "@/models/Users";
 
+export async function getUsersDetails({ search = "", page = 1 }) {
+  await connectDB();
 
-export async function getUsersDetails() {
+  try {
+    const limit = 15;
 
-    await connectDB();
+    const skip = (page - 1) * limit;
 
-    try {
+    let query = {};
 
-        const users = await User.find({})
-        .select("username email image role provider  status createdAt")
-        .sort({createdAt:-1});
+    // console.log("search",search)
 
-        // console.log("users",users)
-
-        return JSON.parse(JSON.stringify(users));
-
-
-    } catch(error) {
-
-        console.error("Error fetching Users: ",error);
-        return[];
-
-
+    if (search) {
+      query = {
+        $or: [
+          { username: { $regex: search, $options: "i" } },
+          { email: { $regex: search, $options: "i" } },
+          { role: { $regex: search, $options: "i" } },
+          { status: { $regex: search, $options: "i" } },
+        ],
+      };
     }
+
+    const users = await User.find(query)
+      .select("username email image role provider  status createdAt")
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    // console.log("users",users)
+    const totalUsers = await User.countDocuments(query);
+
+    const totalPages = Math.ceil(totalUsers / limit);
+
+    return { users: JSON.parse(JSON.stringify(users)), totalPages };
+  } catch (error) {
+    console.error("Error fetching Users: ", error);
+    return [];
+  }
 }
