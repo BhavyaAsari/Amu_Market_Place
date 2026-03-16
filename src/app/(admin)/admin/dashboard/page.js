b40f76dc65761511b188276a0e32ea58935a8246
BHAVYA <bhavy1921@gmail.com>
2026-03-16 16:35:53 +0530

    feat(admin-dashboard): update dashboard page logic and UI

--- src/app/(admin)/admin/dashboard/page.js
+++ src/app/(admin)/admin/dashboard/page.js
@@ -1,9 +1,12 @@
-import { getProductStats } from "@/app/actions/adminActions/getProductStats";
-import { getRevenueData } from "@/app/actions/adminActions/getRevenueData";
-import { getUsersDetails } from "@/app/actions/adminActions/getUserDetails";
-import { getUsersGrowth } from "@/app/actions/adminActions/getUsersGrowth";
-import { getUserStats } from "@/app/actions/adminActions/getUserStats";
+import { getProductStats } from "@/app/actions/productActions/getProductStats";
+import { getRevenueData } from "@/app/actions/productActions/getRevenueData";
+import { getUsersDetails } from "@/app/actions/adminActions/userActions/getUserDetails";
+import { getUsersGrowth } from "@/app/actions/adminActions/userActions/getUsersGrowth";
+import { getUserStats } from "@/app/actions/adminActions/userActions/getUserStats";
+import { getProductAnalytics } from "@/app/actions/productActions/getProductAnalytics";
+
 import AdminLayout from "@/components/admin/AdminLayOut";
+import { getProductDetails } from "@/app/actions/productActions/getProductDetails";
 
 export default async function AdminDashBoard({searchParams}) {
 
@@ -13,11 +16,31 @@ const page = Number(params.page) || 1;
 
 // console.log("search",search)
 
-  const stats = await getProductStats();
-  const revenueData = await getRevenueData();
-  const {users,totalPages} = await getUsersDetails({search,page});
-  const userStats = await getUserStats();
-  const usersGrowth = await getUsersGrowth();
+const [
+  stats,
+  revenueData,
+  usersData,
+  userStats,
+  usersGrowth,
+  productAnalytics,
+  productsData
+] = await Promise.all([
+  getProductStats(),
+  getRevenueData(),
+  getUsersDetails({ search, page }),
+  getUserStats(),
+  getUsersGrowth(),
+ getProductAnalytics(),
+ getProductDetails({search,page})
+
+]);
+
+  const { users, totalPages } = usersData;
+
+  const {productsDetails,productPages} = productsData;
+
+  // console.log("product Data",productsData);
+  // console.log("products Detaisl",productsDetails)
 
   // console.log("user Stats",userStats);
 
@@ -29,6 +52,9 @@ const page = Number(params.page) || 1;
       totalPages={totalPages}
       userStats={userStats}
       usersGrowth={usersGrowth}
+      productAnalytics={productAnalytics}
+      productPages={productPages}
+      productsDetails = {productsDetails}
     />
   );
 }
