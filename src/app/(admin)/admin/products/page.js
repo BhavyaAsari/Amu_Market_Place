c1f6e06ddaa860ab7ff4ca3b3dafa958e2c5ccd2
BHAVYA <bhavy1921@gmail.com>
2026-03-16 16:40:10 +0530

    feat(admin-products): add admin products module

---
+++ src/app/(admin)/admin/products/page.js
@@ -0,0 +1,16 @@
+import ProductSection from "@/components/admin/Product_Segment/productSection";
+import AdminLayout from "../../layout";
+
+export default async  function ProductPage () {
+
+
+    console.log("product Analytics",productAnalytics);
+
+
+    return (
+
+        <>
+       
+        </>
+    )
+}
