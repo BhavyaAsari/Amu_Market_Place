9f69b9c822225f7e2305cea85d4ac0fecf4a50c0
BHAVYA <bhavy1921@gmail.com>
2026-03-19 16:57:29 +0530

    style(chart): redesign custom tooltip with glassmorphism effect

--- src/components/admin/Reusable_Components/AnalyticalChart/customToolTip.js
+++ src/components/admin/Reusable_Components/AnalyticalChart/customToolTip.js
@@ -5,12 +5,25 @@ export default function CustomToolTip ({ active, payload, label, unit}) {
 
      if (active && payload && payload.length) {
 
+       const data = payload[0].payload;
+
+    //  format full date
+    const formattedDate = data?.date
+      ? new Date(data.date).toLocaleDateString("en-US", {
+          weekday: "long",
+          day: "numeric",
+          month: "short",
+          year: "numeric"
+        })
+      : label;
+
          return (
 
         <main className="customToolTipContainer">
 
-            <p className="ToolTipLabel text-gray-700">{label}</p>
-
+              <p className="ToolTipLabel text-gray-700 text-xs">
+          {formattedDate}
+        </p>
             <p className="text-xl font-bold"> {payload[0].value.toLocaleString()}
           <span className="text-xs ml-1 text-white">{unit}</span></p>
 
