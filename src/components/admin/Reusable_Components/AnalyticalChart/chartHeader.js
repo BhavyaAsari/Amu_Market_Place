86b39e665a1f7b8ed6fa657aecc8de0cdb40010b
BHAVYA <bhavy1921@gmail.com>
2026-03-25 16:18:34 +0530

    feat(chart): enhance chart header with improved title and stats display

--- src/components/admin/Reusable_Components/AnalyticalChart/chartHeader.js
+++ src/components/admin/Reusable_Components/AnalyticalChart/chartHeader.js
@@ -1,4 +1,14 @@
+"use client";
+import LocalDropDown from "@/components/productComponents/localDropDown";
+import { useState } from "react";
+
+
 export default function ChartHeader({ title, subtitle }) {
+
+      const [filter, setFilter] = useState("weekly");
+    
+
+    
 
 
     return (
@@ -7,6 +17,8 @@ export default function ChartHeader({ title, subtitle }) {
             <div className="HeaderContainer">
                 <div className="titleContainer ">
                     {/* <span className="text-[22px] tracking-[0.2em] uppercase text-violet-500 font-mono">Analytics</span> */}
+                    
+                              
               
                 </div>
 
