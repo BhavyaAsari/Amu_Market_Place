3afa7c17207fca53dcea81cf0ef74982d67718d7
BHAVYA <bhavy1921@gmail.com>
2026-03-25 16:19:06 +0530

    feat(stats): enhance stats grid UI and data presentation

--- src/components/admin/Reusable_Components/statsGrid.js
+++ src/components/admin/Reusable_Components/statsGrid.js
@@ -12,20 +12,20 @@ export default function StatsGrid({ items }) {
           return (
             <div
               key={i}
-              className="admin-stats-card hover-effect border"
+              className="admin-stats-card hover-effect border border-white/10"
             >
               <div className="flex flex-col ml-2">
-                <p className="font-bold text-purple-300 text-sm textDropShadow">
+                <p className="font-bold text-purple-300 text-xs uppercase tracking-widest  textDropShadow">
                   {item.label}
                 </p>
                 <p className="text-xs font-semibold text-white">{item.subLabel}</p>
 
-                <h3 className="text-xl text-white font-semibold textDropShadow">
+                <h3 className="text-3xl text-white font-semibold textDropShadow">
                   {item.value}
                 </h3>
               </div>
 
-              <Icon size={40} className="Icon-stats" />
+              <div className="bg-white/10 rounded-xl p-3 ml-auto mr-3"> <Icon size={40} className="Icon-stats" /></div>
             </div>
           );
         })}
