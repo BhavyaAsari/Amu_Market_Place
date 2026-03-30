0c4f0083626b21b98ecc2da8939c9c6d091a144f
BHAVYA <bhavy1921@gmail.com>
2026-03-30 16:39:10 +0530

    refactor(charts): improve chart header component

--- src/components/admin/Reusable_Components/AnalyticalChart/chartHeader.js
+++ src/components/admin/Reusable_Components/AnalyticalChart/chartHeader.js
@@ -14,7 +14,7 @@ export default function ChartHeader({ title, subtitle } = {}) {
     return (
 
         <main className="">
-            <div className="HeaderContainer">
+            <div className="HeaderContainer relative -top-20 ">
                 <div className="titleContainer ">
                     {/* <span className="text-[22px] tracking-[0.2em] uppercase text-violet-500 font-mono">Analytics</span> */}
                     
@@ -22,11 +22,11 @@ export default function ChartHeader({ title, subtitle } = {}) {
               
                 </div>
 
-                <h2 className="text-2xl font-extrabold text-white textDropShadow text-glow ">
+                <h2 className="text-2xl font-extrabold text-white textDropShadow text-glow  drop-shadow-[5px_5px_5px_rgba(0,0,0,0.6)]">
                       {title}
                 </h2>
 
-                <p className="text-sm text-gray-800 mt-3 font-bold textDropShadow">{subtitle}</p>
+                <p className="text-sm text-gray-800 mt-3 font-bold textDropShadow ">{subtitle}</p>
 
             </div>
         </main>
