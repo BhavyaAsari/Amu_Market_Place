03867ae1733da9842bbf40fb96f4574e3f470d6f
BHAVYA <bhavy1921@gmail.com>
2026-04-02 16:40:45 +0530

    feat(orders): add admin action to update order status

---
+++ src/app/actions/adminActions/OrdersAction/updateOrderStatus.js
@@ -0,0 +1,62 @@
+"use server";
+
+import { connectDB } from "@/libs/db";
+import Orders from "@/models/Orders";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/authOptions";
+import { logAdminAction } from "@/libs/logger";
+import { revalidatePath } from "next/cache";
+
+
+export async function updateOrderStatus(orderId,newStatus) {
+
+    try {
+
+        await connectDB();
+
+        const session = await getServerSession(authOptions);
+
+        if(!session || session.user.role !== "admin") {
+
+            return {success:false,message:"unauthroized"};
+        }
+
+        const adminId = session.user.id;
+        const adminName = session.user.name;
+
+        const order = await Orders.findById(orderId);
+
+        if(!order) {
+
+            return {success:false,message:"Order not found"};
+        }
+
+        const oldStatus = order.orderStatus;
+
+        //No change 
+        if(oldStatus === newStatus) {
+
+            return {success:false,message:"No change in status"};
+        }
+
+         order.orderStatus = newStatus;
+         await order.save();
+
+        await logAdminAction({
+
+            adminId,
+            adminName,
+            action:"UPDATE_ORDER_STATUS",
+            module:"order",
+            targetId:orderId,
+            before:{orderStatus:oldStatus},
+            after:{orderStatus:newStatus}
+        });
+
+        return {success:true,message:"Order Status updated"};
+    } catch(error) {
+
+        console.error(error);
+        return{success:false,message:"Server error"};
+    }
+}
